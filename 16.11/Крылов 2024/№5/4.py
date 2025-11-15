221342fa491b88818aa81925acfa8abbae5c9895
Dima <streamdima@gmail.com>
2025-11-15 03:41:01 +0300

    .

---
+++ 16.11/Крылов 2024/№5/4.py
@@ -0,0 +1,17 @@
+def cc(x):
+    s = ''
+    while x>0:
+        s = str(x%4) + s
+        x = x//4
+    return s
+for n in range(1,100):
+    b = cc(n)
+    if n%4 == 0:
+        b = b + b[-2:]
+    else:
+        b = b + cc(n%4*2)
+    r = int(b,4)
+    if r >= 1088:
+        print(n,r)
+
+#                  68
---
+++ 16.11/Крылов 2024/№5/5.py
@@ -0,0 +1,16 @@
+for n in range(1,100):
+    b = bin(n) [2:]
+    if n%2==0:
+        b = b + '0'
+    else:
+        b =b + '1'
+    if b.count('1')%3 == 0:
+        b = b.replace(b[:2],'11',1)
+    else:
+        b = b.replace(b[:2],'10',1)
+    r = int(b,2)
+    if r >= 26:   
+        print(n,r)
+
+
+#                        9
